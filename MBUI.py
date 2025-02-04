010aae945a9709ae7ef15557a76801fcfb3d91bc
flotr18 <trouilletflorent@outlook.com>
2025-02-04 09:48:27 +0100

    Fixed User Input

--- MBUI.py
+++ MBUI.py
@@ -1,8 +1,8 @@
 def hello():
     number = input("Enter a number: ")
     i = 0
-    for i in range(number):
+    for i in range(10):
         i += 1
-        print(f"{number}*{i} = {number*i}")
+        print(f"{number} x {i} = {int(number) * i}")
 
 hello()
