ebdc314532de3c3f7f5d73189f44b2ee6e1a1df8
flotr18 <trouilletflorent@outlook.com>
2025-02-04 09:43:35 +0100

    Added User Input

--- MBUI.py
+++ MBUI.py
@@ -1,6 +1,8 @@
-def hello(number):
+def hello():
+    number = input("Enter a number: ")
     i = 0
     for i in range(number):
         i += 1
         print(f"{number}*{i} = {number*i}")
-hello(5)
+
+hello()
