39f0bca65e2d3874fad14d956a945fb87e05ab0a
flotr18 <trouilletflorent@outlook.com>
2025-02-04 09:49:25 +0100

    Change function name

--- MBUI.py
+++ MBUI.py
@@ -1,8 +1,8 @@
-def hello():
+def MultiplicationTable():
     number = input("Enter a number: ")
     i = 0
     for i in range(10):
         i += 1
         print(f"{number} x {i} = {int(number) * i}")
 
-hello()
+MultiplicationTable()
